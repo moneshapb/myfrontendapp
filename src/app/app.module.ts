83e904bd88cb685a794929caacdbd148e5d5f146
moneshapb <120640597+moneshapb@users.noreply.github.com>
2023-05-02 16:43:25 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: HomeComponent },
+      { path: 'search', component: AppComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]), ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
